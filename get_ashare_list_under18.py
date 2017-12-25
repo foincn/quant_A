1799a12aeb784d5605dc71481dfff12e107ff250
tz1006 <a@omg.tf>
2017-12-25 13:01:58 +0800

    Update get_ashare_list_under18.py

--- get_ashare_list_under18.py
+++ get_ashare_list_under18.py
@@ -51,8 +51,73 @@ def insert_data(dbname, formname, code):
     conn.commit()
     conn.close()
 
-#########################
+#######---plot---#######
+import pylab as pl
 
+def plot_ma(TITLE, MA5, MA10, DATE):
+    pl.title(TITLE)
+    a, = pl.plot(DATE, MA5, 'r-')
+    b, = pl.plot(DATE, MA10, 'b-')
+    pl.legend([a, b], ('MA5', 'MA10'), numpoints=1)
+    pl.savefig('stock/%s.png' % TITLE)
+    pl.close()
+    print('Plot %s success' % TITLE)
+
+def get_ma(stock_code):
+    ua_mo = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_1_1 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0 Mobile/15B150 Safari/604.1'
+    header = {'User-Agent':ua_mo}
+    ma_url = 'http://api.finance.ifeng.com/akdaily/?code=%s&type=last' % stock_code
+    now_url = 'http://api.finance.ifeng.com/aminhis/?code=%s&type=five' % stock_code
+    now = requests.get(now_url, headers = header).json()[-1]['record'][-1]
+    ma = requests.get(ma_url, headers = header).json()['record']
+    # MA5
+    ma5_now = float(now[4])
+    ma5_1 = float(ma[-1][8])
+    ma5_2 = float(ma[-2][8])
+    ma5_3 = float(ma[-3][8])
+    ma5_4 = float(ma[-4][8])
+    ma5_5 = float(ma[-5][8])
+    ma5_6 = float(ma[-6][8])
+    ma5_7 = float(ma[-7][8])
+    ma5_8 = float(ma[-8][8])
+    ma5_9 = float(ma[-9][8])
+    ma5_10 = float(ma[-10][8])
+    # MA10
+    ma10_now = float(now[5])
+    ma10_1 = float(ma[-1][9])
+    ma10_2 = float(ma[-2][9])
+    ma10_3 = float(ma[-3][9])
+    ma10_4 = float(ma[-4][9])
+    ma10_5 = float(ma[-5][9])
+    ma10_6 = float(ma[-6][9])
+    ma10_7 = float(ma[-7][9])
+    ma10_8 = float(ma[-8][9])
+    ma10_9 = float(ma[-9][9])
+    ma10_10 = float(ma[-10][9])
+    if ma[-1][0] == now[0].split()[0]:
+        ma5_list = [ma5_1, ma5_2, ma5_3, ma5_4, ma5_5, ma5_6, ma5_7, ma5_8, ma5_9, ma5_10]
+        ma10_list = [ma10_1, ma10_2, ma10_3, ma10_4, ma10_5, ma10_6, ma10_7, ma10_8, ma10_9, ma10_10]
+        date_list = []
+        for i in range(-1, -11, -1):
+            date_list.append(ma[i][0].split('-',1)[1])
+    else:
+        pass
+    return(stock_code, ma5_list, ma10_list, date_list)
+
+def plot_images(CODE):
+    a = get_ma(CODE)
+    plot_ma(a[0], a[1], a[2], a[3])
+
+def plot_list(listname):
+    for i in globals()[str(listname)]:
+        try:
+            plot_images('sh'+i)
+        except:
+            pass
+
+
+#########################
+# 导入A股列表ashare_list并去除20171201以后上市的股票
 def get_stocks_list():
     global ashare_list
     ashare_list = []
@@ -74,7 +139,8 @@ def get_stocks_list():
                 print('%s Added!' % code)
     print('Found %d Stocks!' % len(ashare_list))
 
-def check_stop(stock_code):
+# 去除ashare_list中停牌的股票
+def check_suspended(stock_code):
     print('Checking Suspended %s...' % stock_code)
     s = requests.session()
     s.keep_alive = False
@@ -90,7 +156,60 @@ def check_stop(stock_code):
             if r.text.split("\"")[1].split(",")[12] == '0':
                 ashare_list.remove(stock_code)
 
-# 获取MA数据
+# MA10连续n日大于MA5,导出至watchlist
+def watching(stock_code):
+    print('Watching %s' %stock_code)
+    ua_mo = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_1_1 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0 Mobile/15B150 Safari/604.1'
+    header = {'User-Agent':ua_mo}
+    ma_url = 'http://api.finance.ifeng.com/akdaily/?code=%s&type=last' % sscode(stock_code)
+    ma = requests.get(ma_url, headers = header).json()['record']
+    # MA5
+    ma5_1 = float(ma[-1][8])
+    ma5_2 = float(ma[-2][8])
+    ma5_3 = float(ma[-3][8])
+    ma5_4 = float(ma[-4][8])
+    ma5_5 = float(ma[-5][8])
+    ma5_6 = float(ma[-6][8])
+    ma5_7 = float(ma[-7][8])
+    ma5_8 = float(ma[-8][8])
+    ma5_9 = float(ma[-9][8])
+    ma5_10 = float(ma[-10][8])
+    # MA10
+    ma10_1 = float(ma[-1][9])
+    ma10_2 = float(ma[-2][9])
+    ma10_3 = float(ma[-3][9])
+    ma10_4 = float(ma[-4][9])
+    ma10_5 = float(ma[-5][9])
+    ma10_6 = float(ma[-6][9])
+    ma10_7 = float(ma[-7][9])
+    ma10_8 = float(ma[-8][9])
+    ma10_9 = float(ma[-9][9])
+    ma10_10 = float(ma[-10][9])
+    # 
+    if ma10_1 > ma5_1:
+        if ma10_2 > ma5_2:
+            if ma10_3 > ma5_3:
+                if ma10_4 > ma5_4:
+                    if ma10_5 > ma5_5:
+                        if ma10_6 > ma5_6:
+                            if ma10_7 > ma5_7:
+                                if ma10_8 > ma5_8:
+                                    if ma10_9 > ma5_9:
+                                        if ma10_10 > ma5_10:
+                                            if ma5_2 < ma5_1:
+                                                watchlist.append(stock_code)
+                                                globals()['hist'+str(stock_code)] = [(ma5_1, ma5_2, ma5_3, ma5_4), (ma10_1, ma10_2, ma10_3, ma10_4)]
+                                                print('-----已经添加 %s 至watchlist 并获取MA5/10历史数据-----' % stock_code)
+
+def get_watchlist():
+    global watchlist
+    watchlist = []
+    for i in ashare_list:
+        watching(i)
+    print('wacthlist finished!')
+    print(len(watchlist))
+
+# 获取MA5/10历史数据至hist600000
 def get_ma_hist(stock_code):
     print('Getting %s Data' % stock_code)
     s = requests.session()
@@ -99,17 +218,17 @@ def get_ma_hist(stock_code):
     r = s.get(url, timeout=5)
     ma = r.json()['record']
     # MA5
-    ma5_1 = float(ma[-2][8])
-    ma5_2 = float(ma[-3][8])
+    ma5_1 = float(ma[-1][8])
+    ma5_2 = float(ma[-2][8])
    # MA10
-    ma10_1 = float(ma[-2][9])
+    ma10_1 = float(ma[-1][9])
     if ma5_2 < ma5_1:
         globals()['hist'+str(stock_code)] = (ma5_1, ma10_1)
     else:
         ashare_list.remove(stock_code)
 
 def get_hist_data():
-    for i in ashare_list:
+    for i in watchlist:
         get_ma_hist(i)
     print('All HIST DATA GOT!')
 
@@ -117,7 +236,7 @@ def get_list():
     get_stocks_list()
     a = len(ashare_list)
     for i in ashare_list:
-        check_stop(i)
+        check_suspended(i)
     b = len(ashare_list)
     print(a, b, a-b)
 
@@ -125,6 +244,9 @@ def insert():
     for i in ashare_list:
         insert_data('instance', 'sh_under18', i)
     print('All Data Inserted!')
+
+
+
 
 get_list()
 delete_form('instance', 'sh_under18')
@@ -134,5 +256,6 @@ create_form('instance', 'sh_under18')
 
 t = threading.Thread(target=insert)
 t.start()
-
-get_hist_data()
+get_watchlist()
+#plot_list('watchlist')
+#get_hist_data()
