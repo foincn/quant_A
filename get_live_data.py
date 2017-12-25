e671f91213a5841901deb3f0aa87fe6c4473ba43
tz1006 <a@omg.tf>
2017-12-25 13:35:43 +0800

    Update get_live_data.py

--- get_live_data.py
+++ get_live_data.py
@@ -7,16 +7,13 @@ import requests
 import sqlite3
 import threading
 
-def get_ma_now(stock_code):
-    print('Getting %s Data' % stock_code)
+def price_now(stock_code):
     s = requests.session()
     s.keep_alive = False
-    url =  'http://api.finance.ifeng.com/akdaily/?code=%s&type=last' % sscode(stock_code)
-    r = s.get(url, timeout=5)
-    ma = r.json()['record']
-    # MA5
-    ma5_1 = float(ma[-1][8])
-    ma5_2 = float(ma[-2][8])
-   # MA10
-    ma10_1 = float(ma[-1][9])
-    globals()['dt'+str(stock_code)] = [ma5_1, ma5_2, ma10_1]
+    url = 'http://api.finance.ifeng.com/aminhis/?code=%s&type=five' % sscode(stock_code)
+    now = s.get(now_url, timeout=5).json()[-1]['record'][-1]
+    # Price
+    price = float(now[1])
+    # Average
+    average = float(now[4])
+    return (price, average)
