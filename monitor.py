c3a2cc590c72e5ee329ff0515719545b7ebc6a0c
tz1006 <a@omg.tf>
2017-12-27 17:12:08 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -7,6 +7,7 @@ from multiprocessing import Process
 import time
 from bs4 import BeautifulSoup
 from datetime import date
+from datetime import datetime
 
 ############PROXY###########
 
@@ -332,10 +333,12 @@ def sort_price(share_code, target_price=18):
     price = price_now(share_code)[0]
     if price == '':
         li.remove(share_code)
-        #print('%s 无法获取价格。' % share_code)
+        print('%s 无法获取价格。' % share_code)
     elif price > target_price:
         li.remove(share_code)
-        #print('%s 不符合条件。' % share_code)
+        print('%s 不符合条件。' % share_code)
+    else:
+        print('%s 符合条件!' % share_code)
 
 def sort_price_list(listname='share_list', target_price=18):
     global li
@@ -387,23 +390,35 @@ def sort_ma_list(listname='share_list', days=10):
 
 
 ###########################
+# 监视程序
+# 启动MA监视
 def ma_monitor_start(listname='share_list', count=9999):
     global a
     a = threading.Thread(target=ma_monitor, args=())
     a.start()
 
-
+# MA监视循环
 def ma_monitor(listname='share_list', count=9999):
     global buy_list
+    global stop
+    stop = False
     buy_list = []
+    create_ma_form('MA')
     c = 0
+    print('开始扫描，一共%s次。' % count)
+    start_time = datetime.now()
     while c < count:
-        c += 1
-        print('第%s次扫描, 一共%s支股票，已找到%s支股票符合。' % (c, len(globals()[listname]), len(buy_list)))
-        # time.sleep(10)
-        for i in globals()[listname]:
-            ma_checker(i)
+        while stop != True:
+            c += 1
+            # time.sleep(10)
+            for i in globals()[listname]:
+                ma_checker(i)
+            end_time = datetime.now()
+            timedelsta = (end_time - start_time).seconds
+            print('第%s次扫描, 一共%s支股票，已找到%s支股票符合。 本次扫描耗时%s秒。' % (c, len(globals()[listname]), len(buy_list), timedelsta))
+            end_time = start_time
 
+# Ma监视条件
 def ma_checker(stock_code):
     ma = ma_now(stock_code)
     if ma[0] > ma[1]:
@@ -411,6 +426,43 @@ def ma_checker(stock_code):
             if stock_code not in buy_list:
                 buy_list.append(stock_code)
                 print('%s买入时机' % stock_code)
-    
-    
+                insert_ma_data(stock_code, ma)
 
+# 在数据库'MA'中建立表 '17-12-27'/ CODE/ NAME/ PRICE/ AVERAGE/ TIME/ TIMECROSS/ OTHER
+def create_ma_form(dbname):
+    date = datetime.now().strftime('\"%y-%m-%d\"')
+    conn = sqlite3.connect('database/%s.db' % dbname)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS %s
+        (CODE TEXT PRIMARY KEY UNIQUE,
+        NAME   TEXT,
+        PRICE  TEXT,
+        AVERAGE  TEXT,
+        MA5  TEXT,
+        MA10  TEXT,
+        TIME   TEXT,
+        TIMECROSS   TEXT,
+        OTHER   TEXT);''' % date)
+    conn.commit()
+    conn.close()
+    print("Form %s Created in %s!" % (date, dbname))
+
+# 在数据库'MA'，表'17-12-27'中 写入 / CODE/ NAME/ PRICE/ AVERAGE/ TIME/
+def insert_ma_data(stock_code, ma_now):
+    prices = price_now(stock_code)
+    price = str(prices[0])
+    average = str(prices[1])
+    name = share_name(stock_code)
+    time = datetime.now().strftime('%H:%M:%S')
+    formname = datetime.now().strftime('\"%y-%m-%d\"')
+    conn = sqlite3.connect('database/MA.db')
+    c = conn.cursor()
+    print(stock_code, name, price, average, time)
+    c.execute("INSERT INTO %s (CODE, NAME, PRICE, AVERAGE, TIME) VALUES (?, ?, ?, ?, ?)" % formname,(stock_code, name, price, average, time))
+    conn.commit()
+    conn.close()
+    print('写入 %s 数据成功！' % stock_code)
+
+
+
+    
