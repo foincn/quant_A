61a0c58a57d506786faf0c8f37258575545994db
tz1006 <a@omg.tf>
2017-12-27 19:53:10 +0800

    Update tools.py

--- program/tools.py
+++ program/tools.py
@@ -2,6 +2,7 @@
 
 ########--Tools--########
 
+# sh600001/sz000001
 def sscode(code):
     code = str(code)
     if code[0]+code[1] =='60':
@@ -10,6 +11,7 @@ def sscode(code):
         code = 'sz%s' % code
     return code
 
+# 股票名
 def share_name(code):
     s = requests.session()
     s.keep_alive = False
@@ -19,4 +21,28 @@ def share_name(code):
         r = s.get(url, timeout=2)
     name = r.text.split("\"")[1].split(",",1)[0]
     return name
+
+# 现价／均价
+def price_now(stock_code):
+    s = requests.session()
+    s.keep_alive = False
+    url = 'http://api.finance.ifeng.com/aminhis/?code=%s&type=five' % sscode(stock_code)
+    r = None
+    while r == None:
+        try:
+            r = s.get(url, timeout=3)
+        except:
+            pass
+    if r.text == '':
+        print('无法获取 %s 价格。' % stock_code)
+        price = ''
+        average = ''
+    else:
+        now = r.json()[-1]['record'][-1]
+        # Price
+        price = float(now[1])
+        # Average
+        average = float(now[4])
+    return (price, average)
+
 
